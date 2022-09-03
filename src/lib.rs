711991a4a1d6b973684d0c3b1e679f7b8d092f80
Sebastian Schmidt <isibboi@gmail.com>
2022-09-03 15:53:49 +0300

    Add never function to public interface.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ mod constructors;
 mod triggers;
 
 pub use crate::conditions::{CompiledTriggerCondition, TriggerCondition, TriggerConditionUpdate};
-pub use crate::constructors::{any_n, event_count, geq, none, sequence};
+pub use crate::constructors::{any_n, event_count, geq, never, none, sequence};
 pub use crate::triggers::{
     CompiledTrigger, CompiledTriggers, Trigger, TriggerAction, TriggerEvent, TriggerHandle,
     TriggerIdentifier, Triggers,
